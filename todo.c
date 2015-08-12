0611b475eacb3fc6ebfc611777df0aea3e60ea42
catherinenappington <catherinenappington@openmailbox.org>
2015-08-11 23:53:52 -0700

    Update todo.c
    Removed LinkedList struct and starting coding without modularity in mind. Modularity may come in revisions.

--- todo.c
+++ todo.c
@@ -6,36 +6,24 @@ typedef struct
     Node next;
 } Node;
 
-typedef struct
-{
-
-    Node * first;
-}LinkedList;
-
 Node add(LinkedList * list, string newItem);
 void remove(LinkedList * list, int itemNum);
 void display(LinkedList * list);
 
 int main()
 {
-    LinkedList userList = malloc(sizeof(LinkedList));
-    first = malloc(sizeof(Node));
+    Node first = malloc(sizeof(Node));
     string * inStr = malloc(sizeof(string));
+    Node * ptr = malloc(sizeof(Node));
+    ptr = *head;
 
     printf("To Do List\nWhat needs to be done?\nPress enter after each entry. Press \"0\" when you are done.");
 
     scanf("%s", inStr);
-    Node * ptr = malloc(sizeof(Node));
-    ptr = *head;
     while(inStr != '0')
     {
-        while(ptr.next != '\0')
-            ptr = ptr.next;
-
+        ptr.item = inStr;
         ptr.next = malloc(sizeof(Node));
-        ptr.next.item = inStr;
-        ptr.next.next = '\0';
-
         scanf("%s", inStr);
     }
 
@@ -43,6 +31,7 @@ int main()
 
     free(first);
     free(inStr);
+    free(ptr);
 
     return 0;
 }
